a3eca4a148c3877453e5709a5917a21892e2b254
Georgi Sabev <georgethebeatle@gmail.com>
2019-11-29 17:04:46 +0200

    Make ngrok functions work for zsh

--- config/bash/ngrok.bash
+++ config/bash/ngrok.bash
@@ -1,16 +1,14 @@
-#!/bin/bash
-
 alias gattach='tmux attach -t garden'
 alias gmux='tmux new-session -s garden'
 
 function lsgrok {
-url=$(curl http://localhost:4040/api/tunnels 2>/dev/null | jq -r .tunnels[0].public_url)
+url=$(curl http://localhost:4040/api/tunnels 2>/dev/null | jq -r '.tunnels[0].public_url')
 
-if [ "$url" == "" ] || [ "$url" == "null" ]; then
-  echo No tunnels running?
+if [[ "$url" == "" ]] || [[ "$url" == "null" ]]; then
+  echo "No tunnels running?"
 else
-  host=$(echo "$url" | cut -d / -f 3 | cut -d : -f 1)
-  port=$(echo "$url" | cut -d / -f 3 | cut -d : -f 2)
+  host="$(echo "$url" | cut -d / -f 3 | cut -d : -f 1)"
+  port="$(echo "$url" | cut -d / -f 3 | cut -d : -f 2)"
 
   echo ssh -p "$port" $(whoami)@"$host"
 fi
